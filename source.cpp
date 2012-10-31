94e7e238f6eb25e4118da1d4b27021d2ffb0316c
Christian Bundy <me@christianbundy.com>
2012-10-30 18:08:51 -0700

    Fixed indentation

--- source.cpp
+++ source.cpp
@@ -3,7 +3,7 @@
 #include <string>
 
 int main(int argc, char **argv) {
-  // Find all prime numbers under n 
+	// Find all prime numbers under n 
 	double n = 100; // <- CHANGE ME!
 
 	// All composites will have factors < x
@@ -32,7 +32,7 @@ int main(int argc, char **argv) {
 	}
 
 	// Keep console open
-  system("PAUSE");
-  // Output: 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97 
+	system("PAUSE");
+	// Output: 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97 
 	
 }
