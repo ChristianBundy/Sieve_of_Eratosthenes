ce6df27577d1780ff286c2833ab4e5472de27fa7
Christian Bundy <me@christianbundy.com>
2012-10-30 18:18:11 -0700

    Cleaned up source.cpp
    Removed superfluous comments, changed variable namespace, and removed extra declaration.

--- source.cpp
+++ source.cpp
@@ -3,7 +3,7 @@
 #include <string>
 
 int main(int argc, char **argv) {
-	// Find all prime numbers under n 
+	// Find all prime numbers under n
 	double n = 100; // <- CHANGE ME!
 
 	// All composites will have factors < x
@@ -18,21 +18,17 @@ int main(int argc, char **argv) {
 		if (numbers.at(i) == true) {
 			// Integer is a prime. Find multiples < n
 			for (int j=i; j<(n/i); j++) {
-				int k = j*i;
-				numbers.at(k) = false;
+				numbers.at(j*i) = false;
 			}
 		}
 	}
 	
 	// Print integers marked as primes > 2
-	for (int b = 2; b<n; b++) {
-		if (numbers.at(b) == true) {
-			printf("%d\n", b);
+	for (int k = 2; k<n; k++) {
+		if (numbers.at(k) == true) {
+			printf("%d\n", k);
 		}
 	}
 
-	// Keep console open
-	system("PAUSE");
-	// Output: 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97 
-	
+	system("PAUSE");	
 }
