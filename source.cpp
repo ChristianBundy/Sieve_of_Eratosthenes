ae420b547e4773d6bbfb6a984aa69ca448a1e1f4
Christian Bundy <me@christianbundy.com>
2014-10-07 14:41:06 -0700

    Ask for user input, fix weird std:: bugs

--- source.cpp
+++ source.cpp
@@ -1,34 +1,38 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv) {
-	// Find all prime numbers under n
-	double n = 100; // <- CHANGE ME!
+  using namespace std;
+  // Find all prime numbers under n
+  double n;
+  cout<<"How high do you want to get? ";
+  cin>>n;
 
-	// All composites will have factors < x
-	double x = std::ceil(std::sqrt(n));
+  // All composites will have factors < x
+  double x = ceil(sqrt(n));
 
-	// Vector holds property of primality for each int
-	// Composite: FALSE
-	// Prime:     TRUE (default)
-	std::vector<bool> numbers(n,true);
-	for (int i=2; i<x; i++) {
-		// Check to see if integer is composite.
-		if (numbers.at(i) == true) {
-			// Integer is a prime. Find multiples < n
-			for (int j=i; j<(n/i); j++) {
-				numbers.at(j*i) = false;
-			}
-		}
-	}
-	
-	// Print integers marked as primes > 2
-	for (int k = 2; k<n; k++) {
-		if (numbers.at(k) == true) {
-			printf("%d\n", k);
-		}
-	}
+  // Vector holds property of primality for each int
+  // Composite: FALSE
+  // Prime:     TRUE (default)
+  vector<bool> numbers(n,true);
+  for (int i=2; i<x; i++) {
+    // Check to see if integer is composite.
+    if (numbers.at(i) == true) {
+      // Integer is a prime. Find multiples < n
+      for (int j=i; j<(n/i); j++) {
+        numbers.at(j*i) = false;
+      }
+    }
+  }
 
-	system("PAUSE");	
+  // Print integers marked as primes > 2
+  for (int k = 2; k<n; k++) {
+    if (numbers.at(k) == true) {
+      printf("%d\n", k);
+    }
+  }
 }
